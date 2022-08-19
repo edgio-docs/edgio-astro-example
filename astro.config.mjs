79c16f9496b01c90a31ca9a9bacf8e810c0a5cf1
Rishi Raj Jain <46300090+rishi-raj-jain@users.noreply.github.com>
2022-08-19 19:41:56 +0530

    Remove preact

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,8 @@
 import react from '@astrojs/react'
-import preact from '@astrojs/preact'
 import tailwind from '@astrojs/tailwind'
 import { defineConfig } from 'astro/config'
 
 export default defineConfig({
-  integrations: [preact(), react(), tailwind()],
+  integrations: [react(), tailwind()],
   site: `http://astro.build`,
 })
